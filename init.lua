ee3eccb9ed925ddf2f8e3ba4d4a2f8e9f0f689ea
ShadowGin <Hagelin.Rickard@Gmail.com>
2022-01-02 13:46:12 +0100

    first commit of current nvim settings

---
+++ init.lua
@@ -0,0 +1,11 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.gitsigns"
+require "user.colorscheme"
+print(vim.env.DOTFILES)
+print("test")
---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,10 @@
+local colorscheme = "gruvbox"
+
+vim.g.gruvbox_contrast_dark = "hard"
+vim.g.gruvbox_sign_column = 'bg0'
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
