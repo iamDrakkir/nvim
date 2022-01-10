a4b5c8050222ead28bee2c41083ace00f8680354
Rickard Hagelin <Rickard.Hagelin@ASML.com>
2022-01-10 09:27:03 +0100

    remove lush dependency

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,7 +46,7 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
   -- Colorschemes
-  use {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+  use "ellisonleao/gruvbox.nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
